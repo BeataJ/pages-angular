0d861b62c8bd978e795f4cd8596b33a8f5c12c6e
beata <beata@emc2wave.com>
2021-01-04 16:26:47 -0800

    let key

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -7,6 +7,7 @@ export class ClassDirective {
   constructor(private element: ElementRef) {}
 
   @Input('appClass') set className(classObj: any) {
+    // tslint:disable-next-line:prefer-const
     for (let key in classObj) {
       if (classObj[key]) {
         this.element.nativeElement.classList.add(key);
