7e7f106af55dcebe4dd83ab637522f3f50d879fd
beata <beata@emc2wave.com>
2021-01-04 19:33:10 -0800

    add for loop to create view with template

--- src/app/times.directive.ts
+++ src/app/times.directive.ts
@@ -8,4 +8,11 @@ export class TimesDirective {
     private templateRef: TemplateRef<any>,
     private viewContainerRef: ViewContainerRef
   ) {}
+  @Input('appTimes') set render(times: number) {
+    this.viewContainerRef.clear();
+
+    for (let i = 0; i < times; i++) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef, {});
+    }
+  }
 }
